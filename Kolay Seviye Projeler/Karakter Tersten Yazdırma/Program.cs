05f4e2c943d2d24f8eb8960edd93d0d3533bfaad
Hasibe Akca <hasibetosun42@gmail.com>
2024-02-20 01:04:36 +0300

    Karakteri tersten yazdırma programı tamamlandı.

---
+++ Kolay Seviye Projeler/Karakter Tersten Yazdırma/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Karakter_Tersten_Yazdırma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İstediğiniz cümleyi giriniz : ");
+            string cumle = Console.ReadLine();
+            char[] karakterler = cumle.ToCharArray();
+            for (int i = 1; i < karakterler.Length; i++)
+            {
+                char temp = karakterler[i];
+                karakterler[i] = karakterler[i - 1];
+                karakterler[i - 1] = temp;
+                i++;
+            }
+            if(karakterler.Length %2 != 0)
+            {
+                karakterler[karakterler.Length - 1] = karakterler[karakterler.Length - 2];
+            }
+            foreach (char c in karakterler)
+            { Console.Write(c); }
+        }
+    }
+}
