6e23700b8e32d8f7ab221a69784e33ee59907a69
Hasibe Akca <hasibetosun42@gmail.com>
2024-02-20 00:37:32 +0300

    Algoritma ödevi tamamlandı.

---
+++ Kolay Seviye Projeler/Algoritma/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Algoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir kelime ve bir sayı giriniz aralarını  ',' ile ayırınız: ");
+            string veri = Console.ReadLine();
+            string[] dizi = veri.Split(',');
+            int silinecekIndex = Convert.ToInt32(dizi[1]);
+            string yeniKelime;
+            if (silinecekIndex > dizi[0].Length)
+            {
+                Console.WriteLine(dizi[0]);
+            }
+            else
+            {
+                 yeniKelime = dizi[0].Remove(silinecekIndex, 1);
+                 Console.WriteLine(yeniKelime);
+            }
+          
+
+
+        }
+    }
+}
