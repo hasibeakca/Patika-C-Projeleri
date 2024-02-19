358a013c255e56f913bc9621a408ec9eb1fe9639
Hasibe Akca <hasibetosun42@gmail.com>
2024-02-19 23:52:17 +0300

    Üçgen çizme programı yapıldı.

---
+++ Kolay Seviye Projeler/Üçgen Çizme/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Üçgen_Çizme
+{
+    using System;
+
+    class Program
+    {
+       
+        class Ucgen
+        {
+           
+            private int boyut;
+
+            
+            public Ucgen(int boyut)
+            {
+                this.boyut = boyut;
+            }
+
+            
+            public void UcgenCiz()
+            {
+               
+                for (int i = 1; i <= boyut; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+      
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Üçgenin boyutunu girin: ");
+            int kullaniciBoyut = Convert.ToInt32(Console.ReadLine());
+
+           
+            Ucgen ucgen = new Ucgen(kullaniciBoyut);
+
+           
+            ucgen.UcgenCiz();
+        }
+    }
+
+}
